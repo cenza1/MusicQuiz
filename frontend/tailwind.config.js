1a9f276db190aebc4da0a030bc53c1011c4e33fe
Oscar <oscareinarsson@hotmail.com>
2024-06-26 18:34:59 +0200

    frontend setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  important: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
